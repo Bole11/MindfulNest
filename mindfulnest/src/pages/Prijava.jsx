c52a591a9cf105a46941f9b2708facd402be135b
Bogdan Živanović <125964828+Bole11@users.noreply.github.com>
2025-05-09 17:55:13 +0200

    Prijava Page
    Finished styling, only left to add when the wrong inputs are typed in.

--- mindfulnest/src/pages/Prijava.jsx
+++ mindfulnest/src/pages/Prijava.jsx
@@ -1,10 +1,16 @@
 import { AuthContainer } from "../components/AuthContainer";
-
+import styles from "../styles/Prijava.module.css"
 
 export function Prijava() {
     return (
         <AuthContainer title={"Prijava"}>
-            <form></form>
+            <form className={styles.form}>
+                <input type="email" className={styles.input} placeholder="Unesite svoju e-mail adresu"/>
+                <input type="password" className={styles.input} placeholder="Unesite svoju lozinku"/>
+                <button className={styles.button}>Prijavi se</button>
+                <a className={styles.a}>Zaboravljena lozinka</a>
+                <span className={styles.span}>Nemaš nalog? <a className={styles.a}>Registruj se</a></span>
+            </form>
         </AuthContainer>
     )
 }
